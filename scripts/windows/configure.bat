952f003b7a79eefc5757bd40422a16f1ed063a13
PMichalik <paul.michalik@harman.com>
2018-02-13 16:16:27 +0100

    Adjusted configure.bat

--- scripts/windows/configure.bat
+++ scripts/windows/configure.bat
@@ -1,9 +1,9 @@
 @echo off
 
 setlocal
-call "%~dp0install-deps-windows.bat" %*
+call "%~dp0bootstrap.bat" %*
 
-set "CMakeGeneratorName=Visual Studio 14 2015"
+set "CMakeGeneratorName=Visual Studio 15 2017 Win64"
 
 if "%Platform%"=="x86" (
     if "%Toolset%"=="v140" set "CMakeGeneratorName=Visual Studio 14 2015"
@@ -15,11 +15,12 @@ if "%Platform%"=="x64" (
     if "%Toolset%"=="v141" set "CMakeGeneratorName=Visual Studio 15 2017 Win64"
 )
 
-set "BuildDir=%~dp0..\products\cmake.msbuild.windows.%Platform%.%Toolset%"
+set "BuildDir=%~dp0..\..\products\cmake.msbuild.windows.%Platform%.%Toolset%"
+
 if not exist "%BuildDir%" mkdir "%BuildDir%"
 pushd "%BuildDir%"
 
-call cmake.exe -G "%CMakeGeneratorName%" -DG2O_BUILD_APPS=ON -DG2O_BUILD_EXAMPLES=ON -DSUITE_SPARSE_ROOT="%VcPkgDir%\installed\%VcPkgTriplet%" -DVCPKG_TARGET_TRIPLET=%VcPkgTriplet% -DCMAKE_TOOLCHAIN_FILE="%VcPkgDir%\scripts\buildsystems\vcpkg.cmake" "%~dp0.."
+call cmake.exe -G "%CMakeGeneratorName%" -DG2O_BUILD_APPS=ON -DG2O_BUILD_EXAMPLES=ON" -DVCPKG_TARGET_TRIPLET=%VcPkgTriplet% -DCMAKE_TOOLCHAIN_FILE="%VcPkgDir%\scripts\buildsystems\vcpkg.cmake" "%~dp0..\.."
 
 popd
 endlocal & set "BuildDir=%BuildDir%" & set "BuildType=%BuildType%"

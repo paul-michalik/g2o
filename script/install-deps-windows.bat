24db3a771df48fe26192e433504f4829597b97f2
PMichalik <paul.michalik@harman.com>
2018-02-15 17:27:39 +0100

    Reverted install-deps-windows.bat

--- script/install-deps-windows.bat
+++ script/install-deps-windows.bat
@@ -1,55 +1,28 @@
 @echo off
 
-set "Platform=x64"
-set "Toolset=v141"
-set "BuildType=Release"
-
-if NOT "%~1"=="" set "Platform=%~1"
-if NOT "%~2"=="" set "Toolset=%~2"
-if NOT "%~3"=="" set "BuildType=%~3"
+setlocal
 
 rem ----------------------------------
-rem Locate vcpkg using environment variables
+rem Locate vcpkg using environment variables falling back to sensible defaults
 rem ----------------------------------
-set "VcPkgDir=%~d0\Software\vcpkg\vcpkg"
-set "VcpkgTriplet=%Platform%-windows"
+set "VcPkgDir=%USERPROFILE%\.vcpkg\vcpkg"
+set "VcPkgTriplet=x64-windows"
 if defined VCPKG_ROOT_DIR if /i not "%VCPKG_ROOT_DIR%"=="" set "VcPkgDir=%VCPKG_ROOT_DIR%"
-if defined VCPKG_DEFAULT_TRIPLET if /i not "%VCPKG_DEFAULT_TRIPLET%"=="" set "VcpkgTriplet=%VCPKG_DEFAULT_TRIPLET%"
-
-rem ----------------------------------
-rem Try to look for vcpkg at default locations
-rem ----------------------------------
-if not exist "%VcPkgDir%" set "VcPkgDir=%~d0\Software\vcpkg\vcpkg"
-if not exist "%VcPkgDir%" set "VcPkgDir=%~d0\.vcpkg\vcpkg"
-if not exist "%VcPkgDir%" set "VcPkgDir=C:\Software\vcpkg\vcpkg"
-if not exist "%VcPkgDir%" set "VcPkgDir=C:\.vcpkg\vcpkg"
-if not exist "%VcPkgDir%" set "VcPkgDir=%USERPROFILE%\.vcpkg\vcpkg"
-if not exist "%VcPkgDir%" (
-    echo vcpkg not found, installing at %VcPkgDir%...
-    git clone --recursive https://github.com/Microsoft/vcpkg.git "%VcPkgDir%"
-    call "%VcPkgDir%\bootstrap-vcpkg.bat"
-) else (
-    echo vcpkg found at %VcPkgDir%...
-
-    rem
-    rem Check whether we have a difference in the toolsrc folder. If non empty, %errorlevel% should be 0  
-    rem git diff --name-only origin/HEAD remotes/origin/HEAD | find "toolsrc/" > NUL & echo %errorlevel%
-    rem Put this to local function or better script...
-    rem
-
-    pushd "%VcPkgDir%"
-    git pull --all --prune
-    popd
-
-    rem
-    rem only invoke when changes to "toolsrc/" were made
-    rem 
-    rem call "%VcPkgDir%\bootstrap-vcpkg.bat"
-)
+if defined VCPKG_DEFAULT_TRIPLET if /i not "%VCPKG_DEFAULT_TRIPLET%"=="" set "VcPkgTriplet=%VCPKG_DEFAULT_TRIPLET%"
+    
+pushd %VcPkgDir%
 
 rem ==============================
 rem Upgrade and Install packages.
 rem ==============================
-set "VcPkgDeps=eigen3 suitesparse openblas clapack ceres"
-call "%VcPkgDir%\vcpkg.exe" upgrade %VcPkgDeps% --no-dry-run --triplet %VcPkgTriplet%
-call "%VcPkgDir%\vcpkg.exe" install %VcPkgDeps% --triplet %VcPkgTriplet%
+set "VcPkgLibs=eigen3 suitesparse clapack openblas ceres"
+
+echo vcpkg found at %VcPkgDir%...
+echo installing %VcPkgLibs% for triplet %VcPkgTriplet%...
+
+call vcpkg upgrade %VcPkgLibs% --no-dry-run --triplet %VcPkgTriplet%
+call vcpkg install %VcPkgLibs% --triplet %VcPkgTriplet%
+
+popd
+
+endlocal & set "VcPkgDir=%VcPkgDir%" & set "VcPkgTriplet=%VcPkgTriplet%"

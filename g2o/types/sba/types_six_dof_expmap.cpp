ffae9d545d5f3f91419711570168f78ac97cb4e6
Shanmukha <shanmukhananda@gmail.com>
2018-03-07 11:56:08 +0530

    removed drawTriangles

--- g2o/types/sba/types_six_dof_expmap.cpp
+++ g2o/types/sba/types_six_dof_expmap.cpp
@@ -139,22 +139,6 @@ HyperGraphElementAction* VertexSE3ExpmapWriteGnuplotAction::operator()(HyperGrap
 }
 
 #ifdef G2O_HAVE_OPENGL
-static void drawTriangle(float xSize, float ySize){
-  Vector3F p[3];
-  glBegin(GL_TRIANGLES);
-  p[0] << 0., 0., 0.;
-  p[1] << -xSize, ySize, 0.;
-  p[2] << -xSize, -ySize, 0.;
-  for (int i = 1; i < 2; ++i) {
-    Vector3F normal = (p[i] - p[0]).cross(p[i+1] - p[0]);
-    glNormal3f(normal.x(), normal.y(), normal.z());
-    glVertex3f(p[0].x(), p[0].y(), p[0].z());
-    glVertex3f(p[i].x(), p[i].y(), p[i].z());
-    glVertex3f(p[i+1].x(), p[i+1].y(), p[i+1].z());
-  }    
-  glEnd();
-}
-
 VertexSE3ExpmapDrawAction::VertexSE3ExpmapDrawAction(): DrawAction(typeid(VertexSE3Expmap).name()){
   _cacheDrawActions = 0;
 }
